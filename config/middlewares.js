663c4fcdc1858c102f01d35c7fd5ff8d283a00d8
Gustavo Elesbão <elesbao.gustavo321@gmail.com>
2022-11-07 00:10:46 -0300

    fix: selects bug

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,17 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::body",
+    config: {
+      jsonLimit: "64mb",
+    },
+  },
 ];
